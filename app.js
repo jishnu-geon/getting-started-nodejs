06e3a737d3254686e9e1c19b18e4099556f1219a
Rishabh Tayal <135199635+tayalrishabh96@users.noreply.github.com>
2025-01-03 10:43:06 +0530

    Update app.js
    modifying the logs throttling delay from 2s to 5s

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ setInterval(function(str1, str2) {
   //console.log(logs[i])
   i++
   if (i == logs.length) i = 0
-}, 2000, "Hello.", "How are you?");
+}, 5000, "Hello.", "How are you?");
 
 function makeid(length) {
   var text = "";
